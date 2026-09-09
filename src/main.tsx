b961779073440007fae8a887b1647686b8170c96
Jorge Marquez <104573069+jm181987@users.noreply.github.com>
2026-09-09 14:35:52 -0300

    Initialize payment provider security before app render

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+import { installPaymentSecurity } from "@/lib/paymentSecurityBootstrap";
 
+installPaymentSecurity();
 createRoot(document.getElementById("root")!).render(<App />);
